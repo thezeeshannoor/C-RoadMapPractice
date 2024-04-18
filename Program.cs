43f771df826d5ef71b057671e080734c0912846d
NoorSir <zeeshannoorofficial@gmail.com>
2024-04-18 22:33:18 +0500

    Value Type using struct

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ValueTypeNameSpace;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ValueTypePrac valueTypeObj1 ,valueTypeObj2 = new ValueTypePrac();
+        valueTypeObj1.age = 10;
+        valueTypeObj1.name = "zeeshan";
+        
+        valueTypeObj2 = valueTypeObj1;
+        valueTypeObj1.age = 20;
+        Console.WriteLine(valueTypeObj1.age +" "+ valueTypeObj1.name);
+        Console.WriteLine(valueTypeObj2.age + " " + valueTypeObj2.name);
+    }
+}
---
+++ ValueTypePrac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeNameSpace
+{
+    internal struct ValueTypePrac
+    {
+        public int age;
+        public string name;
+    }
+}
